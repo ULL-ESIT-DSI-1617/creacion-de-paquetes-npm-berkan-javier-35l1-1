34f37512e3d904308d8119819fdfa2352845f89b
berkanrhdz <alu0100815733@ull.edu.es>
2017-04-24 19:25:50 +0100

    Creado Readme.md y arregladas pruebas.

--- test/test.js
+++ test/test.js
@@ -1,12 +1,22 @@
-var Shape = require('../shapesarea.js');
+var Shape = require('../lib/shapesarea.js');
+var should = require('should');
 
-describe("getArea", function() {
-    it("must compute the triangle area correctly. Shape object", function() {
-        let a = new Shape({
-            width: 100,
-            height: 50
-        }, 'Triangle');
-        let s = a.getArea();
-        s.should.match(/^5000$/);
-    })
+let triangle = new Shape({
+    width: 100,
+    height: 50
+}, 'Triangle');
+
+describe("classShape", function() {
+    it("triangle should exist", function() {
+        triangle.should.exist;
+    });
+    it("triangle should be a shape", function() {
+      triangle.should.be.an.instanceof(Shape);
+    });
+    it("width should be 100", function() {
+        triangle.width.should.equal(100);
+    });
+    it("triangle should have height", function() {
+        triangle.should.have.property('height');
+    });
 });
