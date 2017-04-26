acb2e15d51cf1887b6fac8f79c98ef06a7c67cca
Javier <alu0100819441@ull.edu.es>
2017-04-26 13:06:11 +0100

    Crea ejecutable

---
+++ bin/main.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+let util = require('util');
+var readline = require('readline');
+let Shape = require('../shapesarea.js')
+
+try {
+  let type = process.argv[2];
+  let Module = require('@ull-berkan-javier/ull-shape-' + type.toLowerCase());
+  let arg = process.argv[3];
+  let obj = JSON.parse(arg.toString());
+  let t = new Shape(obj, type);
+  let s = t.getArea();
+  console.log(s);
+}
+catch (e) {
+  console.log(e.message);
+}
